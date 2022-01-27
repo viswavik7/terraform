74dbf17c0455fe48d87e5737f7415e8593fc1715
viswavik7 <98520860+viswavik7@users.noreply.github.com>
2022-01-27 20:05:33 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -1,4 +1,12 @@
 terraform {
+  
+    backend "azurerm" {
+    resource_group_name  = "rg-terraformstate"
+    storage_account_name = "terrastatestorage2134"
+    container_name       = "terraformdemo"
+    key                  = "dev.terraform.tfstate"
+  }
+  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -6,6 +14,8 @@ terraform {
     }
   }
 }
+
+
 provider "azurerm" {
   features {}
 }
